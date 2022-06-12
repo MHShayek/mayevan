e428b310f64dd6224d2e9049e940684f33c199e9
Mahedi Shayek <alfmhs@gmail.com>
2022-06-12 19:18:48 +0600

    local hero

--- myapp/settings.py
+++ myapp/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
